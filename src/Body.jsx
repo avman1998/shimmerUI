fd727fa0aac5316731e0e4772e4af66355172a79
Aman <avman1998@gmail.com>
2025-09-04 14:10:28 +0530

    Infinite scroll

--- src/Body.jsx
+++ src/Body.jsx
@@ -3,28 +3,45 @@ import MemeCard from "./MemeCard";
 import ShimmerUI from "./ShimmerUI";
 
 function Body() {
-  const [memes, setMemes] = useState(null);
+  const [memes, setMemes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     async function fetchData() {
+      console.log("fetch");
+      if (isLoading) return;
       try {
-        const data = await fetch("https://meme-api.com/gimme/20");
+        setIsLoading(true);
+        const data = await fetch("https://meme-api.com/gimme/10");
         const json = await data.json();
-        setMemes(json.memes);
+        setMemes((prev) => [...prev, ...json.memes]);
       } catch (error) {
         console.error("ERROR: ", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    function handleScroll() {
+      if (
+        //buffer of 200px
+        window.scrollY + window.innerHeight + 200 >=
+        document.body.scrollHeight
+      ) {
+        fetchData();
       }
     }
     fetchData();
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
   return (
     <div className="flex flex-wrap">
-      {!memes ? (
-        <ShimmerUI />
-      ) : (
-        memes.map((meme) => {
-          return <MemeCard meme={meme} />;
-        })
-      )}
+      {memes?.map((meme, id) => {
+        return <MemeCard key={id} meme={meme} />;
+      })}
+      {isLoading && <ShimmerUI />}
     </div>
   );
 }
